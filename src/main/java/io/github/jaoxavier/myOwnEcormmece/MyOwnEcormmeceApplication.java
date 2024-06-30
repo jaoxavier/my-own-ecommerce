5fd14d23077284806a9a56d3da42dfe0ea222e5c
jaoxavier <jvxavier.lima67@gmail.com>
2024-06-30 15:36:36 -0300

    Start Project

---
+++ src/main/java/io/github/jaoxavier/myOwnEcormmece/MyOwnEcormmeceApplication.java
@@ -0,0 +1,13 @@
+package io.github.jaoxavier.myOwnEcormmece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOwnEcormmeceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOwnEcormmeceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jaoxavier/myOwnEcormmece/MyOwnEcormmeceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jaoxavier.myOwnEcormmece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOwnEcormmeceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
